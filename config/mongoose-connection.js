5b321faaf50a57e40bbca1ac57db82e854da031b
archanaprabhat <archanaprabhath2002@gmail.com>
2024-07-08 20:15:06 +0530

    Routing

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
+const config = require('config');
 const dbgr = require('debug')("development:mongoose");
-;
 
-mongoose.connect("mongodb://127.0.0.1:27017/scatch")
+
+mongoose.connect(`${config.get("MONGODB_URI")}/scatch`)
 .then(function() {
-    console.log("Connected to MongoDB");
+    dbgr("Connected to MongoDB");
 })
 .catch(function(err) {
-    console.log("Could not connect to MongoDB: ", err);
+    dbgr("Could not connect to MongoDB: ", err);
 })
 
 module.exports = mongoose.connection;
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,10 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const ownerModel = "../models/owner-model";
 
-router.get("/", function ( req, res) {
-    res.send("hey");
-})
-
-
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async function (req, res) {
+    let owners = await ownerModel.find();
+    if (owners.length > 0) {
+      return res
+        .send(503)
+        .send("You dont have permission to create  a new owner");
+    }
+    let {fullname, email,password} = req.body;
+    
+    let createdOwner = await ownerModel.create({
+        fullname,
+        email,
+        password
+    })
+    res.status(201).send("we can create a new owner")
+  });
+}
+router.get("/", function (req, res) {
+  res.send("hey");
+});
 
 module.exports = router;
